5fe57ce27c17bd46fdce3352254e053fd54a72a4
Alejo Flores Lucey <alejo@misterflores.com>
2023-10-05 12:20:45 -0300

    Add example view for GET and POST clients

--- turtle_inc/api/views.py
+++ turtle_inc/api/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hola</h1>")
+
+class Clients(APIView):
+    def get(self, request):
+        # TODO
+        return Response({"Hello": "World"}, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        # TODO
+        return Response({"Hello": "World"}, status=status.HTTP_201_CREATED)
