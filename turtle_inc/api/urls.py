5c70345456f42c51e201db0e9389356bc6d17c90
Alejo Flores Lucey <alejo@misterflores.com>
2023-10-07 11:56:38 -0300

    Implement client/<id> endpoints.
    Co-authored-by: Andy <acarro@itba.edu.ar>
    Co-authored-by: Nehui <nllanos@itba.edu.ar>

--- turtle_inc/api/urls.py
+++ turtle_inc/api/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path("clients", Clients.as_view(), name="clients"),
+    path("clients/<int:id>", ClientInformation.as_view(), name="client_information")
 ]
--- turtle_inc/api/views.py
+++ turtle_inc/api/views.py
@@ -2,6 +2,7 @@ import os
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import Cliente, Producto
 from .persistence import PostgresPersistence, MongoPersistence
 
 
@@ -20,3 +21,26 @@ class Clients(APIView):
            data.get("activo") is None:
             return Response({}, status=status.HTTP_400_BAD_REQUEST)
         return Response(persistence.new_client(data.get("nombre"), data.get("apellido"), data.get("direccion"), data.get("activo")), status=status.HTTP_201_CREATED)
+
+
+class ClientInformation(APIView):
+    def get(self, request, id):
+        try:
+            return Response(persistence.get_client(id), status=status.HTTP_200_OK)
+        except Cliente.DoesNotExist:
+            return Response({}, status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, id):
+        try:
+            data = request.data
+            if len(data) == 0:
+                return Response({}, status=status.HTTP_204_NO_CONTENT)
+            return Response(persistence.update_client(id, data.get("nombre"), data.get("apellido"), data.get("direccion"), data.get("activo")), status=status.HTTP_200_OK)
+        except Cliente.DoesNotExist:
+            return Response({}, status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, id):
+        try:
+            return Response(persistence.delete_client(id), status=status.HTTP_200_OK)
+        except Cliente.DoesNotExist:
+            return Response({}, status=status.HTTP_404_NOT_FOUND)
